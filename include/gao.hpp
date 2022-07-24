b5c54d6adbaa708525bfedb6da16590fbcc66811
Adrián Gómez Brandón <adriangbrandon@gmail.com>
2022-07-24 12:53:33 -0400

    New gao

--- include/gao.hpp
+++ include/gao.hpp
@@ -52,7 +52,6 @@ namespace ring {
             typedef uint64_t size_type;
             typedef struct {
                 var_type name;
-                size_type n_triples;
                 size_type weight;
                 std::unordered_set<var_type> related;
             } info_var_type;
@@ -74,13 +73,11 @@ namespace ring {
                 if(it == hash_table.end()){
                     info_var_type info;
                     info.name = var;
-                    info.n_triples = 1;
                     info.weight = size;
                     vec.emplace_back(info);
                     hash_table.insert({var, vec.size()-1});
                 }else{
                     info_var_type& info = vec[it->second];
-                    ++info.n_triples;
                     if(info.weight > size){
                         info.weight = size;
                     }
@@ -93,17 +90,19 @@ namespace ring {
 
                 auto pos_var = hash_table[var];
                 vec[pos_var].related.insert(rel);
+                auto pos_rel = hash_table[rel];
+                vec[pos_rel].related.insert(var);
             }
 
             void fill_heap(const var_type var,
-                           std::unordered_map<var_type, size_type> &hash_table,
+                           const std::unordered_map<var_type, size_type> &hash_table,
                            std::vector<info_var_type> &vec,
                            std::vector<bool> &checked,
                            min_heap_type &heap){
 
-                auto pos_var = hash_table[var];
+                const auto pos_var = hash_table[var];
                 for(const auto &e : vec[pos_var].related){
-                    auto pos_rel = hash_table[e];
+                    const auto pos_rel = hash_table[e];
                     if(!checked[pos_rel]){
                         heap.push({vec[pos_rel].weight, e});
                         checked[pos_rel] = true;
@@ -142,28 +141,27 @@ namespace ring {
                 for (const triple_pattern& triple_pattern : *m_ptr_triple_patterns) {
                     size_type size = util::get_size_interval(triple_pattern, m_ptr_ring);
                     var_type var_s = -1, var_p = -1, var_o = -1;
-                    std::vector<var_type> rel;
                     if(triple_pattern.s_is_variable()){
                         var_s = (var_type) triple_pattern.term_s.value;
                         var_to_vector(var_s, size,hash_table_position, var_info);
-                        rel.push_back(var_s);
                     }
                     if(triple_pattern.p_is_variable()){
                         var_p = (var_type) triple_pattern.term_p.value;
                         var_to_vector(var_p, size,hash_table_position, var_info);
-                        rel.push_back(var_p);
                     }
                     if(triple_pattern.o_is_variable()){
                         var_o = triple_pattern.term_o.value;
                         var_to_vector(var_o, size,hash_table_position, var_info);
-                        rel.push_back(var_o);
                     }
 
-                    for(size_type i = 0; i < rel.size(); ++i){
-                        for(size_type j = 0; j < rel.size(); ++j){
-                            if(i == j) continue;
-                            var_to_related(rel[i], rel[j], hash_table_position, var_info);
-                        }
+                    if(var_s != -1 && var_p != -1){
+                        var_to_related(var_s, var_p, hash_table_position, var_info);
+                    }
+                    if(var_s != -1 && var_o != -1){
+                        var_to_related(var_s, var_o, hash_table_position, var_info);
+                    }
+                    if(var_p != -1 && var_o != -1){
+                        var_to_related(var_p, var_o, hash_table_position, var_info);
                     }
                 }
                 //std::cout << "Done. " << std::endl;
@@ -184,7 +182,7 @@ namespace ring {
                     if(!checked[i]){
                         m_gao.push_back(var_info[i].name); //Adding var to gao
                         checked[i] = true;
-                        if(var_info[i].n_triples > 1){
+                        if(var_info[i].related.size() > 1){
                             min_heap_type heap; //Stores the variables that are related with the chosen ones
                             auto var_name = var_info[i].name;
                             fill_heap(var_name, hash_table_position, var_info, checked,heap);
