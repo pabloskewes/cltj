68c00243e092d07a60ef4b17dfe3b6c9612d389e
Adrián Gómez Brandón <adriangbrandon@gmail.com>
2022-07-28 12:20:20 -0400

    New plots

--- plots/plot.py
+++ plots/plot.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df_data = pd.read_csv('refactoring.csv', sep=';')
-print(df_data)
-print(df_data.head())
-axes = df_data.boxplot(column=['Orig', 'V1', 'V2', 'V3'], grid=False, return_type='axes')
+print(df_data.describe())
+#print(df_data.head())
+axes = df_data.boxplot(column=['Orig/V3'], grid=False, return_type='axes')
 #axes = df_data.boxplot(column=['Intersection'], grid=False, return_type='axes')
 
-plt.ylim(0, 40000000)
-plt.ylabel("Time (ns)")
+#plt.ylim(0, 65000000)
+#plt.ylabel("Time (ns)")
+plt.ylabel("Ratio")
 plt.xlabel("")
 plt.show()
 
