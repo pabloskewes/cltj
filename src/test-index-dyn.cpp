5ce27b2ffc9c6635f5d3440eaf3e999f3d371ef6
adrian <adriangbrandon@gmail.com>
2024-10-08 12:27:00 +0200

    Output in test-index-dyn.cpp

--- src/test-index-dyn.cpp
+++ src/test-index-dyn.cpp
@@ -42,40 +42,89 @@ int main(int argc, char **argv) {
     uint64_t block_size = D.size() / 10;
     //sdsl::memory_monitor::stop();
     for (uint64_t beg = 0; beg < D.size(); beg += block_size) {
-        std::cout << "Insert [" << beg << ", " << beg + block_size - 1 << "]" << std::endl;
-        for (uint64_t i = beg; i < beg + block_size; ++i) {
+        uint64_t last = std::min(beg+block_size, D.size()-1);
+        std::cout << "=======================================" << std::endl;
+        std::cout << "Working with range [" << 0 << ", " << last - 1 << "]" << std::endl;
+        std::cout << "\r insertions: 0% (0/" << last-beg << ")" << std::flush;
+        auto start = timer::now();
+        for (uint64_t i = beg; i < last; ++i) {
             index.insert(D[i]);
+            if((i-beg) % ((last-beg)/1000) == 0) {
+                float per = ((i-beg) / (float) (last-beg+1)) * 100;
+                std::cout << "\r insertions: " << per <<  "% (" << (i-beg) << "/" << last-beg << ")" << std::flush;
+            }
         }
-        std::cout << "Check Insert [" << beg << ", " << beg + block_size - 1 << "]" << std::endl;
+        auto stop = timer::now();
+        auto ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cout << " insertions: 100% (" << last-beg << "/" << last-beg << ") takes" << ms_time << " ms" << std::endl;
+
+        std::cout << "\r check_insertions: 0% (0/" << last-beg << ")" << std::flush;
+        start = timer::now();
         for (uint64_t i = 0; i < queries; ++i) {
-            auto q = rand() % block_size + beg;
+            auto q = std::min(rand() % block_size + beg, D.size());
             auto r = index.test_exists(D[q]);
             if (r < 6) {
                 std::cout << "Error looking for (" << D[q][0] << ", " << D[q][1] << ", " << D[q][2] << ") appears in "
                         << r << "tries." << std::endl;
                 exit(0);
             }
+            if((i-beg) % 1000 == 0) {
+                float per = ((i-beg) / (float) (last-beg+1)) * 100;
+                std::cout << "\r check_insertions:: " << per <<  "% (" << (i-beg) << "/" << last-beg << ")" << std::flush;
+            }
         }
-        std::cout << "Remove [" << beg << ", " << beg + block_size - 1 << "]" << std::endl;
-        for (uint64_t i = beg; i < beg + block_size; ++i) {
+        stop = timer::now();
+        ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cout << "check_insertions: 100% (" << last-beg << "/" << last-beg << ") takes" << ms_time << " ms" << std::endl;
+
+        std::cout << "\r remove: 0% (0/" << last-beg << ")" << std::flush;
+        start = timer::now();
+        for (uint64_t i = beg; i < last; ++i) {
             index.remove(D[i]);
+            if((i-beg) % 1000 == 0) {
+                float per = ((i-beg) / (float) (last-beg+1)) * 100;
+                std::cout << "\r remove: " << per <<  "% (" << (i-beg) << "/" << last-beg << ")\r" << std::flush;
+            }
         }
-        std::cout << "Check Remove [" << beg << ", " << beg + block_size - 1 << "]" << std::endl;
+        stop = timer::now();
+        ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cout << "remove: 100% (" << last-beg << "/" << last-beg << ") takes" << ms_time << " ms" << std::endl;
+
+        std::cout << "\r check_remove: 0% (0/" << last-beg << ")" << std::flush;
+        start = timer::now();
         for (uint64_t i = 0; i < queries; ++i) {
-            auto q = rand() % block_size + beg;
+            auto q = std::min(rand() % block_size + beg, D.size()-1);
             auto r = index.test_exists(D[q]);
             if (r > 0) {
                 std::cout << "Error looking for (" << D[q][0] << ", " << D[q][1] << ", " << D[q][2] << ") appears in "
                         << r << "tries." << std::endl;
                 exit(0);
             }
+            if((i-beg) % 1000 == 0) {
+                float per = ((i-beg) / (float) (last-beg+1)) * 100;
+                std::cout << "\r check_remove: " << per <<  "% (" << (i-beg) << "/" << last-beg << ")" << std::flush;
+            }
         }
-        for (uint64_t i = beg; i < beg+block_size; ++i) {
+        stop = timer::now();
+        ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cout << "check_remove: 100% (" << last-beg << "/" << last-beg << ") ms=" << ms_time << std::endl;
+
+        std::cout << "\r insertions: 0% (0/" << last-beg << ")\r" << std::flush;
+        start = timer::now();
+        for (uint64_t i = beg; i < last; ++i) {
             index.insert(D[i]);
+            if((i-beg) % 1000 == 0) {
+                float per = ((i-beg) / (float) (last-beg+1)) * 100;
+                std::cout << "\r insertions: " << per <<  "% (" << (i-beg) << "/" << last-beg << ")" << std::flush;
+            }
         }
+        stop = timer::now();
+        ms_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cout << "insertions: 100% (" << last-beg << "/" << last-beg << ") takes" << ms_time << " ms" << std::endl;
+        std::cout << "size: " << sdsl::size_in_bytes(index) << " bytes" << std::endl;
+        std::cout << "=======================================" << std::endl << std::endl;
     }
 
-    std::cout << "Size: " << sdsl::size_in_bytes(index) << std::endl;
     sdsl::store_to_file(index, index_name);
     return 0;
 }
