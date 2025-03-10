c5daf5f54d0616fa45efbc9f5b9bace5ef776267
adrian <adriangbrandon@gmail.com>
2025-03-10 21:13:12 +0100

    IMP: printing louds

--- src/test/cltj_louds.cpp
+++ src/test/cltj_louds.cpp
@@ -23,20 +23,9 @@ int main(int argc, char *argv[]) {
     cltj::compact_ltj graph;
     sdsl::load_from_file(graph, index);
 
-   const sdsl::bit_vector &bv = graph.tries[0].bv;
-    uint64_t m = 0;
-    for(uint64_t i = 0; i < bv.size(); ++i){
-        if(bv[i]){
-            m++;
-        }
-    }
+    const sdsl::bit_vector &bv = graph.tries[0].bv;
     std::ofstream ofs(spo_file);
     sdsl::write_member(bv.size(), ofs);
-    sdsl::write_member(m, ofs);
-    for(uint64_t i = 0; i < bv.size(); ++i){
-        if(bv[i]){
-            sdsl::write_member(i, ofs);
-        }
-    }
+    bv.write_data(ofs);
     ofs.close();
 }
