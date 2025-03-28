9d0b6527c9a094294f1907c2b5dac8e0829033db
adrian <adriangbrandon@gmail.com>
2025-03-28 16:34:54 +0100

    IMP: indels

--- src/bench/bench-indels-xcltj.cpp
+++ src/bench/bench-indels-xcltj.cpp
@@ -1,109 +1,131 @@
 //
-// Created by adrian on 13/1/25.
+// Created by adrian on 11/12/24.
 //
 
-
-
+#include <cltj_config.hpp>
+#include <cstdint>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <triple_pattern.hpp>
 #include <index/cltj_index_metatrie_dyn.hpp>
+#include <util/file_util.hpp>
 #include <util/rdf_util.hpp>
+#include <query/ltj_algorithm.hpp>
+#include <index/cltj_index_spo_dyn.hpp>
 
-std::vector<cltj::spo_triple> get_triples(const std::string &updates) {
-
-    std::ifstream ifn(updates);
-    std::string line;
+typedef struct {
     cltj::spo_triple triple;
-    std::vector<cltj::spo_triple> triples;
-    while (std::getline(ifn, line)) {
-        triple = util::rdf::ids::get_triple(line);
-        triples.push_back(triple);
-    }
-    ifn.close();
-    return triples;
+    bool insert; //true => insert; false => delete
+} update_type;
 
+typedef std::vector<ltj::triple_pattern> query_type;
+
+//updates per query >= 1
+template<class index_scheme_type, class trait_type>
+void query_indel(const std::string &index, const std::vector<query_type> &queries, const std::vector<update_type> &updates,
+           const uint64_t limit) {
+
+    typedef ltj::ltj_iterator_lite<index_scheme_type, uint8_t, uint64_t> iterator_type;
+    typedef ltj::ltj_algorithm<iterator_type,
+        ltj::veo::veo_adaptive<iterator_type, trait_type> > algorithm_type;
+    //typedef std::vector<typename algorithm_type::tuple_type> results_type;
+    //typedef algorithm_type::results_type results_type;
+    typedef ::util::results_collector<typename algorithm_type::tuple_type> results_type;
+
+    index_scheme_type graph;
+    sdsl::load_from_file(graph, index);
+    std::cout << "Index loaded : " << sdsl::size_in_bytes(graph) << " bytes." << std::endl;
+
+    uint64_t i_up = 0; //index of update
+    uint64_t nQ = 0;
+    for(auto &q : queries) {
+        //Before each query run upq updates
+            const auto &u = updates[i_up];
+            if(u.insert) {
+                auto start = std::chrono::high_resolution_clock::now();
+                graph.insert(u.triple);
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+                cout << "I" << ";" << i_up << ";" << 0 << ";" << time << endl;
+
+            }else {
+                auto start = std::chrono::high_resolution_clock::now();
+                graph.remove(u.triple);
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+                cout << "D" << ";" << i_up << ";" << 0 << ";" << time << endl;
+            }
+            ++i_up;
+
+        results_type res;
+
+        auto start = std::chrono::high_resolution_clock::now();
+        algorithm_type ltj(&q, &graph);
+        ltj.join(res, limit, 600);
+        auto stop = std::chrono::high_resolution_clock::now();
+
+        auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+        cout << "Q" << ";" << nQ << ";" << res.size() << ";" << time << endl;
+        nQ++;
+    }
 }
 
-std::vector<cltj::spo_triple> get_triples(const std::string &updates, const uint64_t ntriples) {
-
-    std::ifstream ifn(updates);
-    std::string line;
-    cltj::spo_triple triple;
-    std::vector<cltj::spo_triple> triples;
-    while (std::getline(ifn, line) && triples.size() < ntriples) {
-        triple = util::rdf::ids::get_triple(line);
-        triples.push_back(triple);
-    }
-    ifn.close();
-    return triples;
-
-}
-
-uint64_t remove(cltj::compact_ltj_metatrie_dyn &cltj, const std::vector<cltj::spo_triple> &triples) {
-    std::cout << "Removing triples" << std::endl;
-    uint64_t i = 0;
-    auto start = std::chrono::high_resolution_clock::now();
-    for(const auto &triple : triples){
-        i += cltj.remove(triple);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Removal time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns" << std::endl;
-    return i;
-}
-
-uint64_t insert(cltj::compact_ltj_metatrie_dyn &cltj, const std::vector<cltj::spo_triple> &triples) {
-    std::cout << "Inserting triples" << std::endl;
-    uint64_t i = 0;
-    auto start = std::chrono::high_resolution_clock::now();
-    for(const auto &triple : triples){
-        i += cltj.insert(triple);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Insertion time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns" << std::endl;
-    return i;
-}
-
-void insert_out(cltj::compact_ltj_metatrie_dyn &cltj, const std::vector<cltj::spo_triple> &triples) {
-    std::cout << "Inserting triples" << std::endl;
-    uint64_t i = 0;
-    auto start = std::chrono::high_resolution_clock::now();
-    for(const auto &triple : triples){
-        cltj.insert(triple);
-        ++i;
-        if(i % 100000 == 0){
-            std::cout << "Inserted " << i << " triples" << std::endl;
-            auto end = std::chrono::high_resolution_clock::now();
-            std::cout << "Insertion time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()/(i+1) << " ns/query" << std::endl;
-
+void add_queries(const std::string &from, std::vector<query_type> &queries) {
+    std::vector<std::string> str_queries;
+    bool result = ::util::file::get_file_content(from, str_queries);
+    if(result) {
+        for(const auto &q : str_queries) {
+            std::unordered_map<std::string, uint8_t> hash_table_vars;
+            query_type query;
+            std::vector<std::string> tokens_query = ::util::rdf::tokenizer(q, '.');
+            for (std::string &token: tokens_query) {
+                auto triple_pattern = ::util::rdf::ids::get_triple_pattern(token, hash_table_vars);
+                query.push_back(triple_pattern);
+            }
+            queries.push_back(query);
         }
     }
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Insertion time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns" << std::endl;
-
 }
 
-int main(int argc, char *argv[]) {
+void add_updates(const std::string &from, std::vector<update_type> &updates) {
+    std::vector<std::string> lines;
+    bool result = ::util::file::get_file_content(from, lines);
+    if(result) {
+        for(uint64_t i = 0; i < lines.size(); i+=2) {
+            update_type update;
+            update.insert = (lines[i] == "Insert");
+            update.triple = ::util::rdf::ids::get_triple(lines[i+1]);
+            updates.push_back(update);
+        }
+    }
+}
 
-    if (argc != 4) {
-        std::cout << "Usage: " << argv[0] << " <index> <updates> <split>" << std::endl;
+int main(int argc, char **argv) {
+    if(argc != 7){
+        std::cout<< argv[0] << " <index> <queries> <updates> <limit> <type>" << std::endl;
         return 0;
     }
 
-    std::string index = argv[1];
-    std::string insertions = argv[2];
-    bool split = std::stoi(argv[3]);
+    std::string index   = argv[1];
+    std::string queries = argv[2];
+    std::string updates = argv[3];
+    uint64_t limit      = atoll(argv[4]);
+    std::string type    = argv[5];
 
-    std::vector<cltj::spo_triple> triples = get_triples(insertions);
-    std::cout << "Triples: " << triples.size() << std::endl;
-    cltj::compact_ltj_metatrie_dyn cltj;
-    sdsl::load_from_file(cltj, index);
-    if(split) cltj.split();
-    cltj.check_leaves();
-    auto r = remove(cltj, triples);
-    std::cout << "removed: " << r << std::endl;
-    cltj.flatten();
-    if(split) cltj.split();
-    auto i = insert(cltj, triples);
-    std::cout << "inserted: " << i << std::endl;
+
+    std::cout << "===================" << std::endl;
+    std::cout << "Index: " << index << std::endl;
+    std::cout << "Queries: " << queries << std::endl;
+    std::cout << "Updates: " << updates << std::endl;
+    std::cout << "Limit: " << limit << std::endl;
+    std::cout << "===================" << std::endl << std::endl;
+
+    std::vector<query_type> qs;
+    std::vector<update_type> us;
+    add_queries(queries, qs);
+    add_updates(updates, us);
+    query_indel<cltj::compact_ltj_metatrie_dyn, ltj::util::trait_size>(index, qs, us, limit);
+
 
 }
